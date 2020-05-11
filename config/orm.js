6b2b90516d3fcd2d4d0f8b0751d86a7e27f3e9e8
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-11 00:37:00 -0400

    corrected methods

--- config/orm.js
+++ config/orm.js
@@ -5,8 +5,32 @@ class ormBurger{
         this.burgername = burgername
         this.devoured - devoured
     }
-        
+    static async selectAll () {
+        const [rows] = await connection.query(`SELECT * FROM burgers;`)
+        return rows
+      }
+    
+    
+      async insertOne () {
+        const sql = `INSERT INTO cats (burgername, devoured) VALUES (?, ?)`
+        const [result] = await connection.query(sql, [this.burgername, this.devoured])
+        this.id = result.insertId
+        return this
+      }
+    
+      async updateOne () {
+        // ensure sleepy is a valid Boolean
+        this.devoured = fixBool(this.devoured)
+        const sql = `UPDATE burgers SET ? WHERE id = ?`
+        await connection.query(sql, [
+          { burgername: this.burgername, devoured: this.devoured },
+          this.id
+        ])
+        return this
+      }
+    
     }
+    
 
 module.exports = ormBurger
 
