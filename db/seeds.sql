676e1bd98426308a66484f35d4b48541f678e5c4
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-10 22:37:08 -0400

    created and added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krusty Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Homemade Burger', FALSE);
