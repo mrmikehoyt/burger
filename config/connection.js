6d38d362639cfc596e97737a7496662e26b33965
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-11 23:52:47 -0400

    make it better

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 //to require mysql
 const mysql = require("mysql");
 //code to connect node to mysql database burgers_db
+if (process.env.JAWSDB_URL){
+  const connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else {
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "securepassword",
   database: "burgers_db"
 });
-
+};
 
 connection.connect(function(err) {
   if (err) {
