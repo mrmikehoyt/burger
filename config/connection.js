b6c5514d0955f020f123ea87c7ce39ccb2bdce14
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-12 23:44:04 -0400

    troubleshoot heroku

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,21 @@
 //to require mysql
-const mysql = require("mysql");
+var mysql = require("mysql");
+var connection;
 //code to connect node to mysql database burgers_db
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+ connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "securepassword",
+  database: "burgers_db"
+  
+})
 
-  const connection = mysql.createConnection(process.env.JAWSDB_URL)
 
+}
 
 //;
 
