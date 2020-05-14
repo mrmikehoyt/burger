14360e8674c49a09462b447e326a02c5e27bc461
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-13 21:51:27 -0400

    forgot to beautify document

--- config/connection.js
+++ config/connection.js
@@ -2,24 +2,23 @@
 var mysql = require("mysql");
 var connection;
 //code to connect node to mysql database burgers_db . if it's local connects using else
-if (process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
-}
-else {
- connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "securepassword",
-  database: "burgers_db"
-  
-})
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "securepassword",
+    database: "burgers_db"
+
+  })
 
 
 }
 
 
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("if you see this message there is a problem connecting: " + err.stack);
     return;
