8e77fa566f6d059e96ef5123ffe121e8235b5f38
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-13 21:47:32 -0400

    forgot to beautify doc

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -3,21 +3,23 @@ var router = express.Router()
 var burger = require('../models/burger.js')
 const app = express();
 //serves static content from public directory
-app.use(express.static("public"));// http://expressjs.com/en/guide/routing.html
+app.use(express.static("public")); // http://expressjs.com/en/guide/routing.html
 
 // define the home page route
 // u
 router.get('/*', function (req, res) {
- //calls orm selectAll method
+  //calls orm selectAll method
   burger.selectAll(function (data) {
-    
-    var handlebarObject = { burgers: data };
+
+    var handlebarObject = {
+      burgers: data
+    };
     //console logs that information to console
     console.log(handlebarObject);
     //renders the information to index page for handlebars 
     res.render('index', handlebarObject);
 
-    });
+  });
 
 });
 
@@ -26,7 +28,7 @@ router.get('/*', function (req, res) {
 //// by default goes to waiting to be devoured because devoured = false in table
 router.post('/burger/create', function (req, res) {
   //calls insertone model method and passes burger thats from burger_name middleware
-  burger.insertOne(req.body.burger_name, function() {
+  burger.insertOne(req.body.burger_name, function () {
     //redirects page back to /
     res.redirect('/');
   });
@@ -34,13 +36,13 @@ router.post('/burger/create', function (req, res) {
 
 //post method that calls updateone model method with req.body.id middleware that redirects back to /
 router.post('/burger/eat', function (req, res) {
-  burger.updateOne(req.body.id, function() {
+  burger.updateOne(req.body.id, function () {
     res.redirect('/');
   });
 });
 //psot method that calls restoreone model method with req.body.id middleware than redirects back to /
 router.post('/burger/restore', function (req, res) {
-  burger.restoreOne(req.body.id, function() {
+  burger.restoreOne(req.body.id, function () {
     res.redirect('/');
   });
 });
