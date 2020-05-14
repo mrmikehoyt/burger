126960ecdda9db39caf1cb316ef83df796fcd75f
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-13 21:46:09 -0400

    forgot to beautify

--- server.js
+++ server.js
@@ -11,23 +11,25 @@ const PORT = process.env.PORT || 3000;
 //disabled etag validation, for webcache validation. used for heroku troubleshooting 304 / 302 error
 app.set('etag', false);
 //for parsing middleware 
-app.use(bodyParser.urlencoded({ extended: true }));
-// Parses json
-//might not need app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //to use handlebars
 var exphbs = require("express-handlebars");
 //sets default page handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 //contains routes that are used and loads routes under burgers_controller.js
 var burgerroutes = require('./controller/burgers_controller.js')
 
-app.use('/',burgerroutes)
+app.use('/', burgerroutes)
 
 
 // Starts the express server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Logs in console when server has started
   console.log("Listening on: http://localhost:" + PORT);
 });
