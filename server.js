5d4f31b1893dd22ddeb346a0819795c8a9b0d834
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-13 21:44:01 -0400

    added comments

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
+//uesd for web application server
 const express = require('express')
+//used for middleware
 const bodyParser = require("body-parser");
-const connection =require('./config/connection')
+// might not need const connection =require('./config/connection')
 const app = express();
-app.use(express.static("public"));// http://expressjs.com/en/guide/routing.html
+//for serving static files under public
+app.use(express.static("public"));
 // Set the port of our application
 const PORT = process.env.PORT || 3000;
-// Serve static content for the app from the "public" directory in the application directory.
+//disabled etag validation, for webcache validation. used for heroku troubleshooting 304 / 302 error
 app.set('etag', false);
-//app.use(express.static("public"));
-
-
+//for parsing middleware 
 app.use(bodyParser.urlencoded({ extended: true }));
 // Parses json
-app.use(bodyParser.json());
+//might not need app.use(bodyParser.json());
 
+//to use handlebars
 var exphbs = require("express-handlebars");
 //sets default page handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-//used for routes
+//contains routes that are used and loads routes under burgers_controller.js
 var burgerroutes = require('./controller/burgers_controller.js')
 
 app.use('/',burgerroutes)
 
 
-// Start our server so that it can begin listening to client requests.
+// Starts the express server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
+  // Logs in console when server has started
   console.log("Listening on: http://localhost:" + PORT);
 });
