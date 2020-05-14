8e799522d03c06880e170eac695ba80758cd1402
Michael Hoyt <m.mikehoyt@outlook.com>
2020-05-13 21:49:37 -0400

    forgot to beautify document

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,21 @@
 var orm = require('../config/orm.js');
 
 var burger = {
-//selectall  to select all burgers from database using orm selectall a
-  	selectAll: function(cb){
-    	orm.selectAll( 'burgers', cb);     
-  	},
-//insertone to insert burger with burgername from database using insertOne orm 
-  	insertOne: function(burger_name, cb){
-   		orm.insertOne('burgers','burger_name' , burger_name, cb);
-  	},
-//updateOne to update burger_id to devoured. (true) uses orm updateOne        
-  	updateOne: function( burger_id, cb){
-  		orm.updateOne('burgers','devoured',true, burger_id, cb);
-	}, 
+	//selectall  to select all burgers from database using orm selectall a
+	selectAll: function (cb) {
+		orm.selectAll('burgers', cb);
+	},
+	//insertone to insert burger with burgername from database using insertOne orm 
+	insertOne: function (burger_name, cb) {
+		orm.insertOne('burgers', 'burger_name', burger_name, cb);
+	},
+	//updateOne to update burger_id to devoured. (true) uses orm updateOne        
+	updateOne: function (burger_id, cb) {
+		orm.updateOne('burgers', 'devoured', true, burger_id, cb);
+	},
 	//restoreone to update burger_id to not devoured (false) uses updateOne orm
-	restoreOne: function(burger_id, cb) {
-  		orm.updateOne('burgers','devoured',false, burger_id, cb);
+	restoreOne: function (burger_id, cb) {
+		orm.updateOne('burgers', 'devoured', false, burger_id, cb);
 	}
 };
 //exports burger
